ff2f649c8599d3921877e7aa0a8225c5f0cff24a
lucia <llcsaturnaliam@gmail.com>
2024-01-20 20:50:39 -0600

    commented :3

--- dllmain.cpp
+++ dllmain.cpp
@@ -14,19 +14,24 @@ writeFile pWriteFile = nullptr;
 writeFile pWriteFileTarget;
 
 BOOL WINAPI detourWriteFile(HANDLE hFile, LPCVOID lpBuffer, DWORD nNumberOfBytesToWrite, LPDWORD lpNumberOfBytesWritten, LPOVERLAPPED lpOverlapped) {
+    // making sure its not a "turning audio on" event
     if (((const char*)(lpBuffer))[0] != '[') {
         FILE_NAME_INFO fileInfo;
         GetFileInformationByHandleEx(hFile, FileNameInfo, &fileInfo, sizeof(fileInfo));
 
-        std::string buffer;
+        // a lot of useless files have a filename length of 0, so this gets rid of those.
         if (fileInfo.FileNameLength != 0) {
+
+            // gets all the lines into a buffer
+            std::string buffer;
             for (int i = 0; i < nNumberOfBytesToWrite; i++) {
                 buffer += ((const char*)(lpBuffer))[i];
-                std::cout << ((const char*)(lpBuffer))[i];
             }
 
+            // prints out the file and its contents
             std::cout << "\nwriting to save" << ++numFiles << ".dat\n" << buffer << "\n-------\n";
 
+            // saves the file
             std::string filename = "E:\\dev\\pvz-save-hooking\\saves\\save" + std::to_string(numFiles) + ".dat";
             std::ofstream x(filename);
             x << buffer;
@@ -37,30 +42,53 @@ BOOL WINAPI detourWriteFile(HANDLE hFile, LPCVOID lpBuffer, DWORD nNumberOfBytes
     return pWriteFile(hFile, lpBuffer, nNumberOfBytesToWrite, lpNumberOfBytesWritten, lpOverlapped);
 }
 
-void WINAPI panic(std::string message, HMODULE hModule) {
-    MessageBoxA(0, "panicked!", message.c_str(), MB_OK);
+/**
+ * @brief Ejects the thread
+ * @param hModule The HMODULE of the thread
+ * @param fpw The stdout handle
+ */
+void WINAPI eject(HMODULE hModule, FILE* fpw) {
+    fclose(fpw);
+    FreeConsole();
     FreeLibraryAndExitThread(hModule, 0);
 }
 
+/**
+ * @brief Quits the program and displays a message.
+ * @param message The message to be displayed
+ * @param hModule The HMODULE of the thread.
+ */
+void WINAPI panic(std::string message, HMODULE hModule, FILE* fpw) {
+    MessageBoxA(0, "panicked!", message.c_str(), MB_OK);
+    eject(hModule, fpw);
+}
+
+/**
+ * @brief Called when the DLL is injected
+ * @param hModule The DLL's HMODULE
+ */
 BOOL WINAPI dllInjected(HMODULE hModule) {
+    // initial console setup
     AllocConsole();
     FILE* fpw;
     freopen_s(&fpw, "CONOUT$", "w", stdout);
 
     std::cout << "[V] Disable Hook // [E] Enable Hook // [D] Exit\n";
 
+    // initializing minhook
     MH_STATUS status = MH_Initialize();
     if (status != MH_OK) {
         std::string sStatus = MH_StatusToString(status);
-        panic(sStatus, hModule);
+        panic(sStatus, hModule, fpw);
         return FALSE;
     }
 
     if (MH_CreateHookApi(L"Kernel32", "WriteFile", &detourWriteFile, reinterpret_cast<void**>(&pWriteFile)) != MH_OK) {
-        panic("hook failed!", hModule);
+        panic("hook failed!", hModule, fpw);
         return FALSE;
     }
 
+    // silly user interface stuff
     bool hookEnabled = false;
     while (true) {
         if (GetAsyncKeyState('D')) break;
@@ -69,24 +97,23 @@ BOOL WINAPI dllInjected(HMODULE hModule) {
             hookEnabled = true;
             std::cout << "hook enabled!\n";
             if (MH_EnableHook(MH_ALL_HOOKS) != MH_OK) {
-                panic("enable hook failed!", hModule);
+                panic("enable hook failed!", hModule, fpw);
                 return FALSE;
             }
         } else if (GetAsyncKeyState('V') && hookEnabled) {
             hookEnabled = false;
             std::cout << "hook disabled!\n";
             if (MH_DisableHook(MH_ALL_HOOKS) != MH_OK) {
-                panic("disable hook failed!", hModule);
+                panic("disable hook failed!", hModule, fpw);
                 return FALSE;
             }
         }
     }
 
+    // shutting everything down
     MH_DisableHook(MH_ALL_HOOKS);
     MH_Uninitialize();
-    fclose(fpw);
-    FreeConsole();
-    FreeLibraryAndExitThread(hModule, 0);
+    eject(hModule, fpw);
     return TRUE;
 }
 
